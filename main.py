b01c903b81eb942edb3a3452a849a66c83cbbc71
jsherwood00 <142844002+jsherwood00@users.noreply.github.com>
2024-12-07 23:04:34 -0500

    v1 push to remote

--- main.py
+++ main.py
@@ -1,50 +1,39 @@
-from create import print_rows, insert_user_json_data, insert_meta_json_data, size_tables, check_duplicates, destroy_db, create_db
-import glob
+from create import print_rows, insert_user_data, insert_product_data, insert_bundles_data, size_tables, check_duplicates, destroy_db, create_db
 import os
+from glob import glob
 
-def insert_dir_reviews(directory_path: str):
+# Extracts appropriate fields from reviews jsonl file, uploading to `Reviews`
+def insert_from_reviews(directory_path: str):
     file_pattern = os.path.join(directory_path, "*.jsonl") # only jsonl files
-    jsonl_files = glob.glob(file_pattern) # list of all .jsonl in dir
+    jsonl_files = glob(file_pattern) # list of all .jsonl in dir
     
     for json_file in jsonl_files:
         if not os.path.basename(json_file).startswith('meta'): # only reviews
             print(f"Inserting data from {json_file}...")
-            insert_user_json_data(json_file)
+            insert_user_data(json_file)
             
-def insert_dir_meta(directory_path: str):
+# Extracts appropriate fields from meta jsonl file, uploading to `Products``
+# and `Bundles`` tables
+def insert_from_meta(directory_path: str):
     file_pattern = os.path.join(directory_path, "*.jsonl") # only jsonl files
-    jsonl_files = glob.glob(file_pattern) # list of all .jsonl in dir
+    jsonl_files = glob(file_pattern) # list of all .jsonl in dir
     
     for json_file in jsonl_files:
-        # if os.path.basename(json_file).startswith('meta'): # only meta
-        #     print(f"Inserting data from {json_file}...")
-        #     insert_meta_json_data(json_file)
-        # temp fix:
-        print(f"Inserting data from {json_file}...")
-        insert_meta_json_data(json_file)
-        
+        if os.path.basename(json_file).startswith('meta'): # only meta
+            print('Inserting meta from' + json_file)
+            insert_product_data(json_file)
+            insert_bundles_data(json_file)        
 
 if __name__ == "__main__":
 
-    ### UNCOMMENT FOR ALL UPLOADING
-    directory_path = os.getcwd()
+    #directory_path = os.getcwd()
+    #insert_from_reviews(directory_path)
+    #insert_from_meta(directory_path)
     
-    ### UNCOMMENT FOR UPLOADING REVIEWS
-    # insert_dir_reviews(directory_path)
-    
-    ### UNCOMMENT FOR UPLOADING PRODUCT METADATA
-    insert_dir_meta(directory_path)
-    
-    ### Other utility functions
-    # print_rows("products")
-    # print_rows("user_reviews")
+    # print_rows("Reviews")
     size_tables()
-    # check_duplicates()
+    check_duplicates()
     
-    
-    ### Admin utility functions
-    ### WARNING DO NOT USE `destroy_db()`` WITHOUT GROUP APPROVAL ###
-    ### WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNING ###
     # destroy_db()  
-    # create_db()                                                
-    ### WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNING ###
+    # create_db()     
+                                               
